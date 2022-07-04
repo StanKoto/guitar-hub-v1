ffbc8030cf7b68a35442a4fcdd29e60d9d539a3e
stankoto <stankotovych@gmail.com>
2022-07-04 23:57:07 +0300

    Implemented https (with test-version SSL)

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const express = require('express');
+// const https = require('https');
+// const fs = require('fs');
 const path = require('path');
+const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const session = require('express-session');
@@ -26,6 +28,8 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URI)
   .then(res => {
     console.log('MongoDB successfully connected');
+    // const options = { key: fs.readFileSync('key.pem'), cert: fs.readFileSync('cert.pem') };
+    // https.createServer(options, app).listen(PORT, console.log(`Listening on port ${PORT}`));
     app.listen(PORT, console.log(`Listening on port ${PORT}`));
   })
   .catch(err => console.error(err));
