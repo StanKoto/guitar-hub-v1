ad93ae6123a238f8cddc430bb5f70a7bdc74c0f8
stankoto <stankotovych@gmail.com>
2022-07-19 11:59:53 +0300

    Corrected user rating router name

--- routes/userRatingRoutes.js
+++ routes/userRatingRoutes.js
@@ -3,18 +3,18 @@ const { searchResults } = require('../middleware/searchResults');
 const { Rating } = require('../models/Rating');
 const { ratings_get } = require('../controllers/ratingController');
 
-const ratingRouter = express.Router({ mergeParams: true });
+const userRatingRouter = express.Router({ mergeParams: true });
 
-ratingRouter.get('/given-ratings', searchResults(Rating, [
+userRatingRouter.get('/given-ratings', searchResults(Rating, [
   { path: 'tip', select: 'title' }, 
   { path: 'reviewer', select: 'username' },
   { path: 'recipient', select: 'username' }
 ]), ratings_get);
 
-ratingRouter.get('/received-ratings', searchResults(Rating, [
+userRatingRouter.get('/received-ratings', searchResults(Rating, [
   { path: 'tip', select: 'title' }, 
   { path: 'reviewer', select: 'username' },
   { path: 'recipient', select: 'username' }
 ]), ratings_get);
 
-module.exports = ratingRouter;
+module.exports = userRatingRouter;
